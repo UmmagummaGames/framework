a2a984f0fa8fcf116b9d388665513e84a295654e
JoseReyesP <relujo@hotmail.com>
2016-01-02 16:54:11 -0500

    controlador de movimiento

--- BaseTopDownSpaceShip.cs
+++ BaseTopDownSpaceShip.cs
@@ -1,15 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BaseTopDownSpaceShip : MonoBehaviour {
+public class BaseTopDownSpaceShip : ExtendedCustomMonoBehavior {
 
-	// Use this for initialization
-	void Start () {
-	
+	private Quaternion targetRotation;
+
+	private float thePos;
+	private float moveXAmount;
+	private float moveZAmount;
+
+	public float moveXSpeed = 40f;
+	public float moveZSpeed = 15f;
+
+	public float limitX = 15f;
+	public float limtZ = 15f;
+
+	private float originZ;
+
+	[System.NonSerialized]
+	public Keyboard_Input default_input;
+
+	public float horizontal_input;
+	public float vertical_input;
+
+	public virtual void Start()
+	{
+		didInit = false;
+		this.Init();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public virtual void Init()
+	{
+		// referencias cache a nuestros transform y gameobject
+		myTransform = transform;
+		myGO = gameObject;
+		myBody = rigidbody;
+
+		default_input = myGO.AddComponent<Keyboard_Input>();
+
+		originZ = myTransform.localPosition.z;
+
+		didInit = true;
 	}
 }
