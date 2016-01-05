edd9ee22b1fbb5e333b7a1c36aa9dba4d1c15d99
JoseReyesP <relujo@hotmail.com>
2016-01-05 03:55:09 -0500

    añadi mas codigo

--- BaseTopDownSpaceShip.cs
+++ BaseTopDownSpaceShip.cs
@@ -42,4 +42,51 @@ public class BaseTopDownSpaceShip : ExtendedCustomMonoBehavior {
 
 		didInit = true;
 	}
+
+	public virtual void GameStart()
+	{
+		canControl = true;
+	}
+
+	public void GetInput()
+	{
+		horizontal_input = default_input.GetHorizontal();
+		vertical_input = default_input.GetVertical();
+	}
+
+	public virtual void Update()
+	{
+		UpdateShip();
+	}
+
+	public virtual void UpdateShip()
+	{
+		if(!didInit)
+			return;
+		if(!canControl)
+			return;
+
+		GetInput();
+
+		//calcular las cantidades de movimiento para X y Z
+		moveXAmount = horizontal_input * Time.deltaTime * moveXSpeed;
+		moveZAmount = vertical_input * Time.deltaTime * moveZSpeed;
+
+		Vector3 tempRotation = myTransform.eulerAngles;
+		tempRotation.z = horizontal_input * -30f;
+		myTransform.eulerAngles = tempRotation;
+
+		// mover nuestro transform a nuestra posicion actualizada 
+		myTransform.localPosition += new Vector3(moveXAmount,0,moveZAmount);
+
+		// revisar la posicion para asegurar que esta dentro de los limites
+		if(myTransform.localPosition.x <= -limitX || myTransform.localPosition.x >= limitX)
+		{
+			thePos = Mathf.Clamp(myTransform.localPosition.x,-limitX,limitX);
+			myTransform.localPosition = new Vector3(thePos,myTransform.localPosition.y,myTransform.localPosition.z);
+		}
+
+		//tambien revisamos la posicion z
+
+	}
 }
