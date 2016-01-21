62d46c7118405180efe93236d9191037c307ea1c
LuisJoseReyes <quienmatoarogerrabbit@gmail.com>
2016-01-21 14:27:39 -0500

    terminado el script base para control de vehiculo

--- BaseVehicle.cs
+++ BaseVehicle.cs
@@ -96,5 +96,57 @@ public class BaseVehicle : ExtendedCustomMonoBehavior {
 		flatVelo.z = velo.z;
 
 		mySpeed = velo.z;
+
+		if(mySpeed < 2)
+		{
+			if (brake > 0) {
+				rearWheelLeft.motorTorque = -brakeMax * brake;
+				rearWheelRight.motorTorque = -brakeMax * brake;
+
+				rearWheelLeft.brakeTorque = 0;
+				rearWheelRight.brakeTorque = 0;
+
+				frontWheelLeft.steerAngle = steerMax * steer;
+				frontWheelRight.steerAngle = steerMax * steer;
+
+				return;
+			}
+		}
+
+		rearWheelLeft.motorTorque = accelMax * motor;
+		rearWheelRight.motorTorque = accelMax * motor;
+
+		rearWheelLeft.brakeTorque = brakeMax * brake;
+		rearWheelRight.brakeTorque = brakeMax * brake;
+	}
+
+	public void CheckLock()
+	{
+		if (isLocked) {
+
+			// el control esta bloqueado asi que deberiamos detenernos 
+			steer = 0;
+			brake = 0;
+			motor = 0;
+
+			// mantiene nuestro rigidbody en su lugar (pero permite el movimiento en el eje Y de esa manera el vehiculo caeria si no esta sobre el suelo)
+			Vector3 tempVEC = myBody.velocity;
+			tempVEC.x = 0;
+			tempVEC.z = 0;
+			myBody.velocity = tempVEC;
+		}
+	}
+
+	public virtual void GetInput()
+	{
+		// calcula la cantidad de giro del volante
+		steer = Mathf.Clamp(default_input.GetHorizontal(),-1,1);
+
+		// que tanto acelera 
+		motor = Mathf.Clamp(default_input.GetVertical(),0,1);
+
+		// que tanto frena 
+		brake = -1 * Mathf.Clamp(default_input.GetVertical(),-1,0);
 	}
 }
+
